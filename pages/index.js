827009d62011812d27e6c7f2fa7d5195cd5afc76
dithiane <elenamena.colorado@gmail.com>
2023-08-28 14:33:57 -0600

    Added Auth0

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,11 @@
+import Link from "next/link";
 export default function Home() {
-  return <div />;
+  return (
+    <div>
+      <h1>Homepage</h1>
+      <div>
+        <Link href="/api/auth/login">Login</Link>
+      </div>
+    </div>
+  );
 }
